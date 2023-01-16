0d6c168199a976e5473ec159f063450a753fc4d5
drodriguez97 <danrod.biz@gmail.com>
2023-01-16 13:18:34 -0800

    adding Theme.js
    to allow user to toggle between light and dark mode

--- Daniel Portfolio/Experience/Experience.js
+++ Daniel Portfolio/Experience/Experience.js
@@ -6,6 +6,7 @@ import Resources from "./Utils/Resources.js";
 import assets from "./Utils/assets.js";
 
 import Camera from "./Camera.js";
+import Theme from "./Theme.js";
 import Renderer from "./Renderer.js";
 
 import World from "./World/World.js";
@@ -22,6 +23,7 @@ export default class Experience {
         this.time = new Time();
         this.sizes = new Sizes();
         this.camera = new Camera();
+        this.theme = new Theme();
         this.renderer = new Renderer();
         this.resources = new Resources(assets);
         this.world = new World();
